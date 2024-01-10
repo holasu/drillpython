83d5a16a35ec92f7fd38e749f1d62c9d3753ce64
Sudheera Wijedasa <hola.sudheera@gmail.com>
2024-01-10 16:30:16 +0530

    madlib

---
+++ madlibdemo.py
@@ -0,0 +1,6 @@
+name = input("Name: ")
+address = input("Address: ")
+contact = input("Tel: ")
+
+madlib = f"This is {name} and living in {address},\n Please don't hesitate to contact me on {contact}"
+print(madlib)
